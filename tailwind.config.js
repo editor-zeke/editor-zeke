92c575930aca0d45a2d6481864e4f03cf166c960
editor-zeke <ezequiel.designking@gmail.com>
2025-11-28 03:13:58 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+// Archivo: tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./*.{js,ts,jsx,tsx}", // <-- ¡LÍNEA CORREGIDA!
     ],
     theme: {
         extend: {},
